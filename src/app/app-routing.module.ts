3605ec0fa06c0d36c32188789f652f845c2b9993
David Heath <heathdj@gmail.com>
2021-05-28 19:13:30 -0700

    Created Base App Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {SignupComponent} from "./auth/signup/signup.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {ChecklistComponent} from "./checklist/checklist/checklist.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'checklist', component: ChecklistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ChecklistComponent } from './checklist/checklist/checklist.component';
+import { PrepackComponent } from './checklist/prepack/prepack.component';
+import { CustomComponent } from './checklist/custom/custom.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    ChecklistComponent,
+    PrepackComponent,
+    CustomComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
